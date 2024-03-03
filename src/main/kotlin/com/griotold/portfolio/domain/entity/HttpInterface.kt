c94ffdca6a90eadf8da664c7423d1062102e34c9
Griotold <gotjd9773@naver.com>
2024-03-03 18:42:58 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/griotold/portfolio/domain/entity/HttpInterface.kt
+++ src/main/kotlin/com/griotold/portfolio/domain/entity/HttpInterface.kt
@@ -1,12 +1,34 @@
 package com.griotold.portfolio.domain.entity
 
 import jakarta.persistence.*
+import jakarta.servlet.http.HttpServletRequest
 
+/**
+ * HTTP 요청 정보를 담는 엔티티
+ * */
 @Entity
-class HttpInterface : BaseEntity() {
+class HttpInterface(httpServletRequest: HttpServletRequest) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "http_interface_id")
     var id: Long? = null
+
+    var cookies: String? = httpServletRequest.cookies
+        ?.map { "${it.name}:${it.value}" }
+        ?.toString()
+
+    // 요청이 어디서부터 왔는지
+    var referer: String? = httpServletRequest.getHeader("referer")
+
+    var localAddr: String? = httpServletRequest.localAddr
+
+    var remoteAddr: String? = httpServletRequest.remoteAddr
+
+    var remoteHost: String? = httpServletRequest.remoteHost
+
+    var requestUri: String? = httpServletRequest.requestURI
+
+    // 브라우저 정보: 크롬 사파리, 모바일, 데스크탑
+    var userAgent: String? = httpServletRequest.getHeader("user-agent")
 }
--- src/main/kotlin/com/griotold/portfolio/domain/entity/Skill.kt
+++ src/main/kotlin/com/griotold/portfolio/domain/entity/Skill.kt
@@ -1,12 +1,26 @@
 package com.griotold.portfolio.domain.entity
 
+import com.griotold.portfolio.domain.constant.SkillType
 import jakarta.persistence.*
 
 @Entity
-class Skill : BaseEntity() {
+class Skill(
+    name: String,
+    type: String,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
+
+    var name: String = name
+
+    // 문자열을 enum으로 변환해서 넣어준다.
+    @Column(name = "skill_type")
+    @Enumerated(value = EnumType.STRING)
+    var type: SkillType = SkillType.valueOf(type)
+
+    var isActive: Boolean = isActive
 }
