f65c810d4407e1004fbdb4e4e2aae784731d2b26
Griotold <gotjd9773@naver.com>
2024-05-25 18:36:28 +0900

    Presentation - 컨트롤러

--- src/main/kotlin/com/griotold/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/griotold/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,13 +1,47 @@
 package com.griotold.portfolio.presentation.controller
 
+import com.griotold.portfolio.domain.constant.SkillType
+import com.griotold.portfolio.presentation.service.PresentationService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class PresentationViewController {
+class PresentationViewController(
+    private val presentationsService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "test"
+    }
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+
+        val introductions = presentationsService.getIntroductions()
+        model.addAttribute("introductions", introductions)
+
+        val links = presentationsService.getLinks()
+        model.addAttribute("links", links)
+
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model): String {
+        val resume = presentationsService.getResume()
+        model.addAttribute("resume", resume)
+        model.addAttribute("skillTypes", SkillType.values())
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model): String {
+        val projects = presentationsService.getProjects()
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
     }
 }
