ce8493635163bce9147fa215252edade2244a695
Griotold <gotjd9773@naver.com>
2024-06-04 10:27:42 +0900

    인터셉터 개발

--- src/main/kotlin/com/griotold/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/griotold/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,21 @@
 package com.griotold.portfolio.presentation.interceptor
 
+import com.griotold.portfolio.domain.entity.HttpInterface
+import com.griotold.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+): HandlerInterceptor {
+
+    // afterCompletion 같은 경우, 컨트롤러에서 예외를 던져도 항상 실행된다.
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
