d3bc20b02f9381e2f6b0fa299b211e5a0cff4a77
Griotold <gotjd9773@naver.com>
2024-06-04 11:20:19 +0900

    [Admin] 공통 개발 - Exception 과 Advice

--- src/main/kotlin/com/griotold/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/griotold/portfolio/admin/exception/AdminException.kt
@@ -2,9 +2,24 @@ package com.griotold.portfolio.admin.exception
 
 import org.springframework.http.HttpStatus
 
-class AdminException(
+abstract class AdminException(
     httpStatus: HttpStatus,
     message: String
 ) : RuntimeException(message){
     val httpStatus: HttpStatus = httpStatus
 }
+
+class AdminBadRequestException(message: String) : AdminException(
+    httpStatus = HttpStatus.BAD_REQUEST,
+    message = message
+) {
+
+}
+
+class AdminInternalServerErrorException(message: String) : AdminException(
+    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = message
+) {
+
+}
+
